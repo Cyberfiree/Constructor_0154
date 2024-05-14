3b1daf2ee683bf97a8aa24d17b6b6917d85c0e9a
Cyberfire <buayagoreng99@gmail.com>
2024-05-14 15:04:13 +0800

    membuat class mahasiswa menggunakan akses modifier

--- StaticVariabel_0154/StaticVariabel_0154.cpp
+++ StaticVariabel_0154/StaticVariabel_0154.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+
+	// Konstruktor yang menerima nama dan menginisialisasi objek
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
+};
 int main()
 {
 }
