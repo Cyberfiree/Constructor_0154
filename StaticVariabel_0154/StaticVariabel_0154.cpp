2efef50cf4548a3c3786e88d6ee92dd18b1ace34
Cyberfire <buayagoreng99@gmail.com>
2024-05-14 15:04:41 +0800

    membuat metode untuk menetapkan id dan mencetak detail

--- StaticVariabel_0154/StaticVariabel_0154.cpp
+++ StaticVariabel_0154/StaticVariabel_0154.cpp
@@ -16,6 +16,20 @@ public:
 		setID();
 	}
 };
+
+int Mahasiswa::nim = 154;
+
+// Metode untuk menetapkan ID
+void Mahasiswa::setID() {
+	id = ++nim;
+}
+
+// Metode untuk mencetak detail
+void Mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
 int main()
 {
 }
